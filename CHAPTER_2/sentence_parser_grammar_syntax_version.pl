89565e05a800df860187450ac40dace4a1e2e1a1
aa-software2112 <a_dreoli@encs.concordia.ca>
2023-02-23 15:42:07 -0500

    Added AST parameters to CFG in prolog example code

--- CHAPTER_2/sentence_parser_grammar_syntax_version.pl
+++ CHAPTER_2/sentence_parser_grammar_syntax_version.pl
@@ -3,11 +3,20 @@
 % -? [sentence_parser_grammar_syntax_version]
 % then, the sentence can be queried with sentence([the, girl, sang, a, song, "."], []).
 % which will indicate if the grammar contains that phrase
-sentence --> nounPhrase, predicate, ["."].
-nounPhrase --> determiner, noun.
-predicate --> verb, nounPhrase.
-predicate --> verb.
-determiner --> [a].
-determiner --> [the].
-noun --> [boy] ; [girl] ; [cat] ; [telescope] ; [song] ; [feather].
-verb --> [saw] ; [touched] ; [surprised] ; [sang].
+% In this we also added several 
+sentence(sent(N,P)) --> nounPhrase(N), predicate(P), ["."].
+nounPhrase(nounph(D,N)) --> determiner(D), noun(N).
+predicate(pred(V,N)) --> verb(V), nounPhrase(N).
+predicate(pred(V)) --> verb(V).
+determiner(det(a)) --> [a].
+determiner(det(the)) --> [the].
+noun(noun(boy)) --> [boy].
+noun(noun(girl)) --> [girl].
+noun(noun(cat)) --> [cat].
+noun(noun(telescope)) --> [telescope].
+noun(noun(song)) --> [song].
+noun(noun(feather)) --> [feather].
+verb(verb(saw)) --> [saw].
+verb(verb(touched)) --> [telescope].
+verb(verb(surprised)) --> [surprised].
+verb(verb(sang)) --> [sang].
