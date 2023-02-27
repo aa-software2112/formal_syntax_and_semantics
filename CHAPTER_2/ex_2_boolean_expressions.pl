9815e8db50352214202b8a94f37f06443c847650
aa-software2112 <a_dreoli@encs.concordia.ca>
2023-02-27 18:29:20 -0500

    Added initial code for example 2 with both left and right associativity

---
+++ CHAPTER_2/ex_2_boolean_expressions.pl
@@ -0,0 +1,36 @@
+% This example can be found on page 56
+% booleanExpr(bexp(T)) --> bTerm(T).
+% bTerm(term(T)) --> bElem(T); bTerm(T), ["and"], bElem(T).
+
+% This will create a LEFT ASSOCIATIVE TREE
+% This is due to T1 continually being stored in the
+% leftmost part of the tree.
+bTerm(T) --> bElem(T1), restTerm(T1, T).
+
+% Notice BOTH restTerms below create the left-associative tree...
+% Different ways of doing the same thing!
+restTerm(T1, T) --> [and], bElem(T2), restTerm(aand(T1, T2), T).
+restTerm(T, T) --> [].
+
+
+% This will create a RIGHT ASSOCIATIVE TREE
+% The main difference is that this restTermRAssoc rule creates
+% the aand(value, Tree), where the rightmost part of the aand will be
+% filled at lower levels
+bTermRAssoc(T) --> bElem(T1), restTermRAssoc(T1, T).
+restTermRAssoc(T1, aand(T1, T)) --> [and], bElem(T2), restTermRAssoc(T2, T).
+restTermRAssoc(T, T) --> [].
+
+
+bElem(elem(true)) --> [true].
+bElem(elem(false)) --> [false].
+bElem(elem(T)) --> {v(T)}, [T], {write(T)}.
+bElem(compare(T)) --> {comp(T)}, [T], {write(T)}.
+bElem(nt(T)) --> ['not', '('], booleanExpr(T), [')'].
+bElem(T) --> ['('], booleanExpr(T), [')'].
+
+
+v(a).
+v(b).
+comp("==").
+comp("!=").
